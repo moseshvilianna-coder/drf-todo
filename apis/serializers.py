ec2498621963c3451ae503e33ce484f8f79ba6f9
moseshvilianna-coder <moseshvilianna@gmail.com>
2026-02-23 10:40:30 +0000

    preparing for  production

---
+++ apis/serializers.py
@@ -0,0 +1,12 @@
+#apis/serializers.py
+from rest_framework import serializers
+from todo import models
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            "id",
+            "title",
+            "description",
+        )
+        model = models.Todo
---
+++ todo/admin.py
@@ -0,0 +1,6 @@
+#todo/admin.py
+from django.contrib import admin
+
+from todo.models import Todo
+
+admin.site.register(Todo)
